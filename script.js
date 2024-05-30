bdec97b3e6fded932bd3c45ae2d114a6bc36146c
Mohsin Ali <166200483+Mohsinalitari@users.noreply.github.com>
2024-05-30 02:56:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+//Write a js program to find cube of any number using function.
 function cube(number){
     return number*number*number
 }
  var num=parseInt(prompt('Enter the number'));
 var result=cube(num);
 console.log(result);
-//Write a js program to find diameter, circumference and area of circle using functions.
+//02 Write a js program to find diameter, circumference and area of circle using functions.
 function calculatediameter(radius){
     return(2*radius)
 }
@@ -21,3 +22,22 @@ var area = calculatearea(num)
 console.log('Diameter',diameter);
 console.log('Circumference',circumference);
 console.log('Area',area)
+//03 Write a js program to find maximum and minimum between two numbers using functions.
+function maximum(number,number1){
+   return  (number > number1 ? number :number1);
+}
+//find two number between maximum number
+function minimum(number,number1){
+    return  (number < number1 ? number :number1);
+}
+//find two number between Mnimum number
+
+var num = parseInt(prompt('Enter the first number'));
+var num1 =parseInt(prompt('Enter the seocnd numbre'));
+//input from user
+var max= maximum(num,num1);
+var min =minimum(num,num1);
+
+console.log('Maximum Number is  ',max);
+console.log('Minimum Number is  ',min);
+//print data
