3c994f114e772e847c2229c2fd15ff1e16f49523
Mohsin Ali <166200483+Mohsinalitari@users.noreply.github.com>
2024-05-31 03:00:30 -0700

    complete 4

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Write a js program to find cube of any number using function.
+//01 Write a js program to find cube of any number using function.
 function cube(number){
     return number*number*number
 }
@@ -37,7 +37,22 @@ var num1 =parseInt(prompt('Enter the seocnd numbre'));
 //input from user
 var max= maximum(num,num1);
 var min =minimum(num,num1);
-
+//Calculate Even and Odd number
 console.log('Maximum Number is  ',max);
 console.log('Minimum Number is  ',min);
 //print data
+//Write a js program to check whether a number is even or odd using functions.
+function find_even(num) {
+    return num % 2 === 0;  // Check if the remainder is 0 when divided by 2
+}
+
+function find_odd(num) {
+    return num % 2 !== 0;  // Check if the remainder is not 0 when divided by 2
+}
+
+var num = parseInt(prompt('Enter the number'));
+var result = find_even(num);
+var result1 = find_odd(num);
+
+console.log("Is even:", result);
+console.log("Is odd:", result1);
